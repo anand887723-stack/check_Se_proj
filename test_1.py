cf57540f0cbcac02b2b06bb7f86644179b46227e
anand887723-stack <79003838+anand887723-stack@users.noreply.github.com>
2022-11-25 13:22:29 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -4,15 +4,36 @@ import unittest
 from test_p1 import getSum
 
 class test(unittest.TestCase):
-  def test_list_int(self):
-    """
-    Test case 1
-    """
+  def test_1(self):
     data = []
     for  i in range(10):
       data.append(i)
     ans = getSum(data)
     self.assertEqual(ans, 45)
+    
+	def test_2(self):
+    data = [1,2,3,4]
+    
+    ans = getSum(data)
+    self.assertEqual(ans, 10)
+	    
+	def test_3(self):
+    data = [1,2,3,4,5]
+    
+    ans = getSum(data)
+    self.assertEqual(ans, 15)
+	    
+	def test_4(self):
+    data = [1,2,3,4]
+    
+    ans = getSum(data)
+    self.assertEqual(ans, 102)
+	    
+	def test_5(self):
+    data = [1,2,3,4]
+    
+    ans = getSum(data)
+    self.assertEqual(ans, 100)
     
 if __name__== '__main__':
   unittest.main()
