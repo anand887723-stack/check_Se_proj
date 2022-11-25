2d50a96fe3a6faa9e214e90e9a04d18747a7b0d6
anand887723-stack <79003838+anand887723-stack@users.noreply.github.com>
2022-11-25 11:07:34 +0530

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,17 @@
+import unittest
+
+from p1 import getSum
+
+class test(unittest.TestCase):
+  def test_list_int(self):
+    """
+    Test case 1
+    """
+    data = []
+    for  i in range(10):
+      data.append(i)
+    ans = getSum(data)
+    self.assertEqual(ans, 35)
+    
+if _name_ == '_main_':
+  unittest.main()
